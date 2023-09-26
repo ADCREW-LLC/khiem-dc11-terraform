9142dd2f959a13fd24d323dc48be64fab295f0b5
ADCREW LLC <hello@adcrew.us>
2023-09-26 16:46:07 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "khiem-dc11-terraform-s3-state"
+    key    = "networking.tfstate"
+    region = "ap-southeast-2"
+  }
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
