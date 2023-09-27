57dcf0f41434a35c79e2caab58ffeb8b0523f4a8
ADCREW LLC <hello@adcrew.us>
2023-09-27 09:07:26 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-// This should be called - 999
 terraform {
   backend "s3" {
     bucket = "khiem-dc11-terraform-s3-state"
@@ -14,4 +13,10 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile                 = terraform.workspace
+  shared_credentials_file = "/home/ubuntu/.aws/credentials"
+  region                  = terraform.workspace == "default" ? "ap-southeast-2" : terraform.workspace == "test" ? "ap-northeast-3" : "ap-south-1"
 }
