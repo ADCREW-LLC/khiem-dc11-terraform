0be9eb8d0894faa38451d68b4ec0cc72a959a9a2
ADCREW LLC <hello@adcrew.us>
2023-09-27 10:28:09 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    access_key = var.aws_access_key_id
-    secret_key = var.aws_secret_access_key
+    access_key = local.aws_access_key_id
+    secret_key = local.aws_secret_access_key
     bucket = "khiem-dc11-terraform-s3-state"
     key    = "networking.tfstate"
     region = "ap-southeast-2"
@@ -19,7 +19,7 @@ terraform {
 
 provider "aws" {
   profile                 = terraform.workspace
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
+  access_key = local.aws_access_key_id
+  secret_key = local.aws_secret_access_key
   region                  = terraform.workspace == "default" ? "ap-southeast-2" : terraform.workspace == "test" ? "ap-northeast-3" : "ap-south-1"
 }
