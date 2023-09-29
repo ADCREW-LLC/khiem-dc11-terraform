1183afddf6500f5dd7f5dd35f18c46bfa61ab905
ADCREW LLC <hello@adcrew.us>
2023-09-29 17:01:18 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,3 @@
-/*terraform {
-  backend "s3" {
-    access_key = "AKIAWIVUTCVPTL7HHOK3"
-    secret_key = "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"
-    bucket = "khiem-dc11-terraform-s3-state"
-    key    = "networking.tfstate"
-    region = "ap-southeast-2"
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  profile                 = terraform.workspace
-  access_key = "AKIAWIVUTCVPTL7HHOK3"
-  secret_key = "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"
-  region                  = terraform.workspace == "default" ? "ap-southeast-2" : terraform.workspace == "test" ? "ap-northeast-3" : "ap-south-1"
-}
-*/
 terraform {
   required_version = ">= 1.2.0"
 }
