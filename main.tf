71a26d9f3992e082f70e09ccb85436c7c7e20389
ADCREW LLC <hello@adcrew.us>
2023-09-29 10:27:54 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,6 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
 output "hello_world" {
   value = "Hello, World!"
 }
