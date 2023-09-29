4abb88e455701d25f2fdd3ee134dbf6af79b90a2
ADCREW LLC <hello@adcrew.us>
2023-09-29 10:22:10 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-terraform {
-  /*backend "s3" {
+/*terraform {
+  backend "s3" {
     access_key = "AKIAWIVUTCVPTL7HHOK3"
     secret_key = "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"
     bucket = "khiem-dc11-terraform-s3-state"
     key    = "networking.tfstate"
     region = "ap-southeast-2"
-  }*/
+  }
 
   required_providers {
     aws = {
@@ -22,4 +22,16 @@ provider "aws" {
   access_key = "AKIAWIVUTCVPTL7HHOK3"
   secret_key = "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"
   region                  = terraform.workspace == "default" ? "ap-southeast-2" : terraform.workspace == "test" ? "ap-northeast-3" : "ap-south-1"
+}
+*/
+terraform {
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
+}
+
+# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
+output "hello_world" {
+  value = "Hello, World!"
 }
